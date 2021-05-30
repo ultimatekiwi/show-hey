160efe27266592cfabf60f0b8c257fcfaf992988
Ben Corner <bcorner@reed.edu>
2021-05-30 17:58:22 -0500

    first attempt at performing GET with axios
    issues with CORS encountered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rowData: [],
+    };
+  }
+
+  fetchData() {
+    return axios.get();
+  }
+
+  getItems() {
+    axios({
+      method: "get",
+      //   url: "https://mlb21.theshow.com/apis/items",
+      url: "/items.json?type=stadium&page=1",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+    })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.error(err));
+  }
+
+  getListings() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://mlb21.theshow.com/apis/items.json"
+      )
+      .then((response) => {
+        this.setState({
+          rowData: response.data,
+        });
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusTest);
+        console.log(response.headers);
+      });
+  }
+
+  componentDidMount() {
+    this.getListings();
+    // this.getItems();
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
