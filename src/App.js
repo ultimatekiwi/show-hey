c1f14a3cc7ce2f81b69fdcb80495d10ed7981846
Ben Corner <bcorner@reed.edu>
2021-05-31 12:21:28 -0500

    Use cors-anywhere-demo proxy to successfully peform GET /items request

--- src/App.js
+++ src/App.js
@@ -10,23 +10,39 @@ class App extends React.Component {
     };
   }
 
-  fetchData() {
-    return axios.get();
-  }
-
   getItems() {
-    axios({
-      method: "get",
-      //   url: "https://mlb21.theshow.com/apis/items",
-      url: "/items.json?type=stadium&page=1",
-      headers: {
-        "content-type": "application/json",
-        accept: "application/json",
-      },
-    })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
-  }
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://mlb21.theshow.com/apis/items.json"
+      )
+      .then((response) => {
+        this.setState({
+          rowData: response.data,
+        });
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusTest);
+        console.log(response.headers);
+      });
+  }					
+
+
+
+
+
+//   getItems() {
+//     axios({
+//       method: "get",
+//       //   url: "https://mlb21.theshow.com/apis/items",
+//       url: "/items.json?type=stadium&page=1",
+//       headers: {
+//         "content-type": "application/json",
+//         accept: "application/json",
+//       },
+//     })
+//       .then((res) => console.log(res.data))
+//       .catch((err) => console.error(err));
+//   }
 
   getListings() {
     axios
@@ -45,8 +61,8 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getListings();
-    // this.getItems();
+    // this.getListings();
+    this.getItems();
   }
 
   render() {
